1d8c90cfc21c4100cfea3b4008d19a0e4f52b223
jpozo-1nationgfx <jpozo@1nationgfx.com>
2022-01-25 16:25:21 -0500

    change credentials

--- pages/login.jsx
+++ pages/login.jsx
@@ -6,16 +6,24 @@ import cookie from 'js-cookie';
 
 const secret = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiI1ODkzMjY3IiwibmFtZSI6IkJvb2tpbmdSZW5lIiwiaWF0IjoxNTE2MjM5MDIyfQ.ijULaJ83WAhRjDfCfuqco3tSur0BQt5ToAVHkrH3WZw";
 
+const credentials = {
+    username: 'Admin',
+    password: 'Miami2022@'
+}    
+
 const login = () => {
 
     const {setAuth} = useContext(AuthContext);
     const router = useRouter();
-    const [token, setToken] = useState('');
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
-    const handleToken = e => setToken(e.target.value);
+    const handleUser = e => setUsername(e.target.value);
+
+    const handlePass = e => setPassword(e.target.value);
 
     const handleSubmit = () => {
-        if(token === secret){
+        if(username === credentials.username && password === credentials.password){
             cookie.set("token", secret);
             setAuth(true);
             router.push('/');
@@ -25,12 +33,20 @@ const login = () => {
     return (
         <Grid container justifyContent={'center'}>
             <Grid item xs={12} md={6} pt={10}>
+            <TextField
+                    margin={'normal'}
+                    fullWidth
+                    value={username}
+                    onChange={handleUser}
+                    label={'Usuario'}
+                />
                 <TextField
                     margin={'normal'}
                     fullWidth
-                    value={token}
-                    onChange={handleToken}
+                    value={password}
+                    onChange={handlePass}
                     label={'Password'}
+                    type={'password'}
                 />
                 <Button fullWidth variant={'contained'} onClick={handleSubmit}>{'Entrar'}</Button>
             </Grid>
