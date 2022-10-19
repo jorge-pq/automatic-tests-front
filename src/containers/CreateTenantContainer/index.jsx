bd88bd4d6c2524187ba4ef5e0b599cf9ce398937
djorg-solutions <djorg90@gmail.com>
2022-10-19 07:58:04 -0400

    feat: create tenant

--- src/containers/CreateTenantContainer/index.jsx
+++ src/containers/CreateTenantContainer/index.jsx
@@ -1,13 +1,101 @@
 import React from 'react';
-import { Grid } from '@mui/material';
+import { Divider, Grid, TextField, Button } from '@mui/material';
+import { Controller, useForm } from 'react-hook-form';
+import {create} from '../../services/tenant.service';
+
+const errorText = { color: '#E8530E' };
 
 const CreateTenantContainer = () => {
 
-    return (
-        <Grid container pb={4}>
-          crear mayorista o minorista
-        </Grid>
-    );
+  const { register, handleSubmit, formState: { errors }, reset, setValue, control } = useForm();
+
+  const { mutate: createTenant } = useMutation(create, {
+    onSuccess: (data) => {
+        alert("Empresa registrada satisfactoriamente.");
+        reset();
+    },
+    onError: (error) => {
+      alert(error.response.data.message);
+    }
+});
+
+  const onSubmit = (data) => {
+    createTenant(data);
+}
+
+  return (
+    <Grid container p={4}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+      <Divider>{'Empresa'}</Divider>
+      <Grid item xs={12}>
+        <Controller
+          control={control}
+          name="name"
+          rules={{ required: true }}
+          render={({ field }) => <TextField size={'small'} fullWidth placeholder={'Nombre *'} {...field} />}
+        />
+        {
+          errors.name && <label style={errorText}>{'El nombre de la empresa es requerido'}</label>
+        }
+      </Grid>
+
+      <Divider>{'Usuario'}</Divider>
+
+      <Grid item xs={6}>
+        <Controller
+          control={control}
+          name="fullname"
+          rules={{ required: true }}
+          render={({ field }) => <TextField size={'small'} fullWidth placeholder={'Nombre y apellidos *'} {...field} />}
+        />
+        {
+          errors.fullname && <label style={errorText}>{'El nombre completo del usuario es requerido'}</label>
+        }
+      </Grid>
+      <Grid item xs={6}>
+        <Controller
+          control={control}
+          name="username"
+          rules={{ required: true }}
+          render={({ field }) => <TextField size={'small'} fullWidth placeholder={'Nombre de usuario *'} {...field} />}
+        />
+        {
+          errors.username && <label style={errorText}>{'El nombre de usuario es requerido'}</label>
+        }
+      </Grid>
+      <Grid item xs={6}>
+        <Controller
+          control={control}
+          name="phone"
+          rules={{ required: true }}
+          render={({ field }) => <TextField size={'small'} fullWidth placeholder={'Telefono *'} {...field} />}
+        />
+        {
+          errors.phone && <label style={errorText}>{'El telefono es requerido'}</label>
+        }
+      </Grid>
+      <Grid item xs={6}>
+        <Controller
+          control={control}
+          name="password"
+          rules={{ required: true }}
+          render={({ field }) => <TextField size={'small'} fullWidth placeholder={'Contraseña *'} {...field} />}
+        />
+        {
+          errors.password && <label style={errorText}>{'La contraseña es requerida'}</label>
+        }
+      </Grid>
+      <Grid item xs={12}>
+        <Button
+          variant={'contained'}
+          type="submit"
+        >
+          {'Registrar'}
+        </Button>
+      </Grid>
+      </form>
+    </Grid>
+  );
 };
 
 export default CreateTenantContainer;
---
+++ src/services/tenant.service.js
@@ -0,0 +1,7 @@
+import {rget, rpost, rput, rdelete} from '../lib/request';
+import cookie from 'js-cookie';
+
+export const create = async (values) => {
+    const {data} = await rpost('api/tenant/create', values);
+    return data;
+};
