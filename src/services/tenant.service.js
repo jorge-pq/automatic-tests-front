4e96187a0b95a7a1f5ed225814659c703b79a006
djorg-solutions <djorg90@gmail.com>
2022-10-27 20:20:44 -0400

    feat: add user to tenant

--- src/services/tenant.service.js
+++ src/services/tenant.service.js
@@ -6,8 +6,13 @@ export const create = async (values) => {
     return data;
 };
 
-
 export const getTenants = async (jwt) => {
     const {data} = await rget('api/tenant/brokers', jwt);
     return data;
 };
+
+export const addUser = async (values) => {
+    const {data} = await rpost('api/add/user', values, cookie.get("token"));
+    return data;
+};
+
