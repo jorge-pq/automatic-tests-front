5b30dae7fb3be2fe9c66c213b10d9ea682099277
djorg-solutions <djorg90@gmail.com>
2023-01-30 21:21:37 -0500

    comment tree component

--- src/containers/AutomaticTestsContainer/components/Aside.jsx
+++ src/containers/AutomaticTestsContainer/components/Aside.jsx
@@ -1,8 +1,8 @@
 import * as React from 'react';
-import TreeView from '@mui/lab/TreeView';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import ChevronRightIcon from '@mui/icons-material/ChevronRight';
-import TreeItem from '@mui/lab/TreeItem';
+// import TreeView from '@mui/lab/TreeView';
+// import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+// import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+// import TreeItem from '@mui/lab/TreeItem';
 import {useRouter} from 'next/router';
 
 export default function Aside({apps}) {
@@ -14,25 +14,26 @@ export default function Aside({apps}) {
     }
 
     return (
-        <TreeView
-            aria-label="file system navigator"
-            defaultCollapseIcon={<ExpandMoreIcon />}
-            defaultExpandIcon={<ChevronRightIcon />}
-            sx={{ height: 'auto', flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
-            defaultExpanded={['root']}
-        >
-            <TreeItem nodeId="root" label="Applications" defaultChecked={true}>
-                {
-                    apps.map(item =>
-                        <TreeItem nodeId={item.code.toString()} label={item.name}>
-                            {
-                                item.tests.map(t => <TreeItem onClick={()=>goToTest(t._id)} nodeId={t._id.toString()} label={t.description} />)
-                            }
+        // <TreeView
+        //     aria-label="file system navigator"
+        //     defaultCollapseIcon={<ExpandMoreIcon />}
+        //     defaultExpandIcon={<ChevronRightIcon />}
+        //     sx={{ height: 'auto', flexGrow: 1, maxWidth: 400, overflowY: 'auto' }}
+        //     defaultExpanded={['root']}
+        // >
+        //     <TreeItem nodeId="root" label="Applications" defaultChecked={true}>
+        //         {
+        //             apps.map(item =>
+        //                 <TreeItem nodeId={item.code.toString()} label={item.name}>
+        //                     {
+        //                         item.tests.map(t => <TreeItem onClick={()=>goToTest(t._id)} nodeId={t._id.toString()} label={t.description} />)
+        //                     }
                              
-                        </TreeItem>
-                    )
-                }
-            </TreeItem>
-        </TreeView>
+        //                 </TreeItem>
+        //             )
+        //         }
+        //     </TreeItem>
+        // </TreeView>
+        <></>
     );
 }
